eb295cc935c973c78bacee7c74d5d5f37d1835f9
DorokhovaNA <nisan261lib@mail.ru>
2021-02-07 18:29:40 +0300

    удалены фaйлы

--- src/App.js
+++ src/App.js
@@ -1,47 +1,7 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-const example = 'Прекрасный пример!';
-const exampleLogic = true;
-const exampleTernary = false;
+const element = (<div>
+  Hello World!!
+  <a href = "">go to</a>
+</div>);
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p style = {{
-          color: 'blue',
-          shadowSize: 2,
-          fontSize: 55-30
-        }}>
-          Hello World!!
-        </p>
-        <p>{example}</p>
-        <p>
-          {exampleLogic || 'exampleLogic is false'}
-        </p>
-        <p>
-          {exampleTernary ? 'exampleTernary is true' : 'exampleTernary is false'}
-          {undefined}
-          {null}
-          {false}
-          {true}
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+export {element}
