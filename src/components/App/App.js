cea7d89b0312a5af2b179b48f5fbc47bd176a796
DorokhovaNA <nisan261lib@mail.ru>
2021-02-10 21:43:49 +0300

    добавлен App.css

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ItemList from '../ItemList/ItemList';
 import Footer from '../Footer/Footer';
+import './App.css';
 
 const todoItem = 'Проанализировать партнерский и зарплатный канал';
 const App = () => {
@@ -16,8 +17,8 @@ const App = () => {
       }
   ];
   return (
-  <div>
-        <h1>Важные дела</h1>
+  <div className="wrap">
+        <h1 className="wrap__title">Важные дела</h1>
         <ItemList items={items} />
         <Footer count={3} />
   </div>);
