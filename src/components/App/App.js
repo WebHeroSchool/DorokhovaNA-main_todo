505511a175827fd1d46257d4ed0f5e82f40d03f6
DorokhovaNA <nisan261lib@mail.ru>
2021-02-11 23:52:14 +0300

    Material UI

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ItemList from '../ItemList/ItemList';
+import InputItem from '../InputItem/InputItem';
 import Footer from '../Footer/Footer';
 import styles from './App.module.css';
+
 
 const todoItem = 'Проанализировать партнерский и зарплатный канал';
 const App = () => {
@@ -22,6 +24,7 @@ const App = () => {
   return (
   <div className={styles.wrap}>
         <h1 className={styles.title}>Важные дела</h1>
+        <InputItem />
         <ItemList items={items} />
         <Footer count={3} />
   </div>);
