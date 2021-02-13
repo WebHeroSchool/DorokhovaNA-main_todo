1937b39b50fdf93170a41c3d977edaa54e5b222d
DorokhovaNA <nisan261lib@mail.ru>
2021-02-13 20:34:23 +0300

    проведен рефакторинг App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,30 +2,39 @@ import React from 'react';
 import ItemList from '../ItemList/ItemList';
 import InputItem from '../InputItem/InputItem';
 import Footer from '../Footer/Footer';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
 import styles from './App.module.css';
 
-const App = () => {
-  const items = [
-      {
-        value: 'Проанализировать партнерский и зарплатный канал',
-        isDone: true
-      },
-      {
-        value: 'Учесть конвертацию заявок в разрезе каждого канала',
-        isDone: false
-      },
-      {
-        value: 'Подготовить аналитическую отчетность',
-        isDone: true
-      }
-  ];
-  return (
-  <div className={styles.wrap}>
-        <h1 className={styles.title}>Важные дела:</h1>
-        <InputItem />
-        <ItemList items={items} />
-        <Footer count={3} />
-  </div>);
+
+class App extends React.Component {
+  render() {
+    const items = [
+        {
+          value: 'Проанализировать партнерский и зарплатный канал',
+          isDone: true
+        },
+        {
+          value: 'Учесть конвертацию заявок в разрезе каждого канала',
+          isDone: false
+        },
+        {
+          value: 'Подготовить аналитическую отчетность',
+          isDone: true
+        }
+    ];
+    return (
+    <div className={styles.wrap}>
+        <Card variant="outlined">
+            <CardContent>
+                <h1 className={styles.title}>Важные дела:</h1>
+                <InputItem />
+                <ItemList items={items} />
+                <Footer count={3} />
+            </CardContent>
+        </Card>
+    </div>);
   }
+};
 
 export default App;
